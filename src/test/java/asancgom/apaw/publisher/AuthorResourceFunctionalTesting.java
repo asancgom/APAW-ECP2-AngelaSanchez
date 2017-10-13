8de7fd5e147ea8bfeaf43e66c3afadcad18cd859
Angela <Angela@172.16.139.64>
2017-10-13 14:51:43 +0200

    Create Funtional test.

--- src/test/java/asancgom/apaw/publisher/AuthorResourceFunctionalTesting.java
+++ src/test/java/asancgom/apaw/publisher/AuthorResourceFunctionalTesting.java
@@ -1,15 +1,20 @@
 package asancgom.apaw.publisher;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
+
+import asancgom.apaw.publisher.http.HttpClientService;
+import asancgom.apaw.publisher.http.HttpException;
+import asancgom.apaw.publisher.http.HttpMethod;
+import asancgom.apaw.publisher.http.HttpRequest;
+import asancgom.apaw.publisher.http.HttpRequestBuilder;
 
 public class AuthorResourceFunctionalTesting {
 
-    
 
+	    @Test(expected = HttpException.class)
+	    public void testAuthorPublisherSumPublisherIdNotFound() {
+	        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.GET).path("author/{id}/sum").expandPath("3").build();
+	        new HttpClientService().httpRequest(request);
+	    }
 }
